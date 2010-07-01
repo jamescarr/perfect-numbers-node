48e5bd715f69d35b785e8ea8a054b022fd394d42
James Carr <james.r.carr@gmail.com>
2010-06-30 20:12:48 -0500

    added blog edit type functionality

--- app.js
+++ app.js
@@ -25,4 +25,22 @@ get('/', function(){
   })
 })
 
+get('/blog/new', function(){
+  this.render('blog_new.html.haml', {locals:{title:'New Post'}});
+});
+
+post('/blog/new', function(){
+  var self = this;
+  articleProvider.save({
+    title: this.param('title'),
+    body: this.param('body')
+  }, function(error, docs) {
+    self.redirect('/')
+  });
+});
+
+get('/*.css', function(css){
+  this.render(css + ".css.sass", {layout:false});
+});
+
 run();
